c3b28ba8efc1dd7541a34085e899ff52ba797fc1
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 13:40:26 +0600

    Add files via upload

---
+++ strings1_salutation.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string firstName;
+    string lastName;
+    string salutation;
+
+    cout << "Введите имя: ";
+    cin >> firstName;
+    cout << "Введите фамилию: ";
+    cin >> lastName;
+
+    salutation = "Здравствуйте, " + firstName + ' ' + lastName + "!\n";
+    cout << salutation;
+
+    SetConsoleCP(866);
+    SetConsoleOutputCP(866);
+    return 0;
+}
+
