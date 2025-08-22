3581971d4e04a12ba5e8e316dbf5ad2bade9d8b7
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 13:41:00 +0600

    Add files via upload

---
+++ strings2_guessing.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string secretWord = "малина";
+    string userWord;
+    
+    do {
+        cout << "Угадайте слово: ";
+        cin >> userWord;
+        if (userWord != secretWord)
+            cout << "Неправильно" << endl;
+    } while (userWord != secretWord);
+    cout << "Правильно! Вы победили! Загаданное слово - " << secretWord << endl;
+    
+    SetConsoleCP(866);
+    SetConsoleOutputCP(866);
+    return 0;
+}
